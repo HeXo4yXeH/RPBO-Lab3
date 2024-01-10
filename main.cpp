dcb6df58ebd9c1326b0e2d882ae8117501aa562a
HeXo4yXa <91568069+HeXo4yXeH@users.noreply.github.com>
2024-01-10 16:18:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿﻿#include <iostream>
+
+import Math;
+using namespace std;
+int main()
+{
+	double re, im;
+	cout << "Real and Imag num:" << endl;
+	cin >> re >> im;
+	Complex c(re, im);
+
+	double chisl, znam;
+	cout << "Numerator and Denumerator num:" << endl;
+	cin >> chisl >> znam;
+	Rational r(chisl, znam);
+	double d;
+	cout << "double:" << endl;
+	cin >> d;
+
+	Rational answ = f(r);
+	double a = answ.Nominator(), b = answ.Denominator();
+
+	cout << "f(" << c << ") = " << f(c) << endl;
+	cout << "f(" << r << ") = " << f(r) << " = " << a / b << endl;
+	cout << "f(" << d << ") = " << f(d) << endl;
+
+	return 0;
+}
